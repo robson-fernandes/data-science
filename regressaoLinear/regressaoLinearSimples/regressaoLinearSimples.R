ece9ff20628fb8e8053eaa48ea36fa36aa21350d
Robson Fernandes <robson.ux@gmail.com>
2017-02-23 23:30:11 -0300

    Teste de Significância

--- regressaoLinear/regressaoLinearSimples/regressaoLinearSimples.R
+++ regressaoLinear/regressaoLinearSimples/regressaoLinearSimples.R
@@ -2,29 +2,32 @@
 vendas = read.table('vendas.txt', header=T)
 attach(vendas)
 
-#Diagrama de Dispers??o
+#Diagrama de Dispersao
 plot(trimestre, venda, col=c('red', 'blue', 'green'))
 
-#Coeficiente de Correla????o
+#Coeficiente de Correlacao
 cor(trimestre,venda)
 
-#Teste de Hip??teses para a o Coeficiente de Correla????o
+#Teste de Hipoteses para a o Coeficiente de Correlacao
 cor.test(trimestre,venda)
 
-#Ajuste do Modelo de Regress??o Linear
+#Ajuste do Modelo de Regressao Linear
 ajuste.modeloLinear = lm(venda ~ trimestre)
 ajuste.modeloLinear
+
+#Teste de Significancia do Modelo
+summary(ajuste.modeloLinear)
 
 #Teste de Normalidade
 shapiro.test(residuals(ajuste.modeloLinear))
 
-# Previs??o de Vendas
+# Previsao de Vendas
 predict(ajuste.modeloLinear, newdata=data.frame(trimestre=14))
 
-#Reta ajustada no Diagrama de Dispers??o
+#Reta ajustada no Diagrama de Dispersao
 plot(trimestre, venda, col=c('red', 'blue', 'green'))
 abline(ajuste.modeloLinear)
 
-#Coeficiente de Determina????o
+#Coeficiente de Determinacao
 summary(ajuste.modeloLinear)$r.squared
 
