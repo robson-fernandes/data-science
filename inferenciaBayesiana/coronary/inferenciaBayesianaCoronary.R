bfee278b3b7c0a22a3d16fbb6bf5dd083d27994c
Robson Fernandes <robson.ux@gmail.com>
2017-05-01 23:58:17 -0300

    Inferência em Redes Bayesianas
    Coronary

--- inferenciaBayesiana/coronary/inferenciaBayesianaCoronary.R
+++ inferenciaBayesiana/coronary/inferenciaBayesianaCoronary.R
@@ -1,19 +1,29 @@
 require("bnlearn")
 
-setwd("~/Documents/projetos/data-science/inferenciaBayesiana/coronary")
-
 #Coleta da Base de Dados Coronary
 coronaryDataFrame <- data.frame(coronary)
 
 #Aprendizagem da rede bayesiana usando algoritmo Hill-Climbing (HC)
 res <- hc(coronaryDataFrame)
-res <- drop.arc(res, "M..Work", "Family")
-
 #Plot da Rede
 plot(res)
 
+#Remover o Link entre nós "M..Work", "Family"
+res <- drop.arc(res, "M..Work", "Family")
+#Plot da Rede
+plot(res)
 
 #Rede Bayesiana Ajustada
 bnAjustado <- bn.fit(res, data = coronaryDataFrame)
 
-print(bnAjustado$Proteins)
+#Tabela de Probabilidade Condicional - Pressure
+print(bnAjustado$Pressure)
+
+#Inferência em Redes Bayesianas
+cpquery(bnAjustado, 
+        event = (Proteins=="<3"), 
+        evidence = ( Smoking=="no" & Pressure==">140" ) )
+
+
+
+
