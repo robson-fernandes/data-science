6eacd9b61332cc6ea4bd20fd197c03dc5703fc23
Robson Fernandes da Silva <robson.fernandes@linx.com.br>
2017-07-10 19:47:09 -0300

    Matriz de Correlacao

---
+++ deepLearning/matrizCorrelacao.R
@@ -0,0 +1,18 @@
+library(corrplot)
+
+dados = read.table('dados.csv', header=TRUE, sep=";")
+attach(dados)
+
+training.setOriginal <- dados[1:85, ]
+training.set <- training.setOriginal
+training.set$mes <- as.double(training.set$mes)
+training.set <- scale(training.set)
+
+
+M <- cor(training.set)
+#Matriz de correlacao - Color
+corrplot(M, method="color")
+
+#Matriz de correlacao - Numerico
+corrplot(M, method="number")
+
